b701aa94aed0889af470cefaba805738cba3e4b5
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-07-25 13:18:47 +0300

    corrected tests

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -5,21 +5,18 @@ import lombok.val;
 
 import java.util.Locale;
 
-    public class DataGenerator {
-        public DataGenerator() {
-        }
-
-
-        public static Registration.RegistrationUser generateByNamePhoneCity() {
-            Faker faker = new Faker(new Locale("RU"));
-            return new ru.netology. Registration.RegistrationUser(
-                    faker.name().lastName()+" "+faker.name().firstName(),
-                     faker.phoneNumber().phoneNumber(),
-                     faker.address().city());
-
-
-        }
+public class DataGenerator {
+    public DataGenerator() {
     }
+
+    public static Registration.RegistrationUser generateByNamePhoneCity() {
+        Faker faker = new Faker(new Locale("RU"));
+        return new ru.netology.Registration.RegistrationUser(
+                faker.name().lastName() + " " + faker.name().firstName(),
+                faker.phoneNumber().phoneNumber(),
+                faker.address().city());
+    }
+}
 
 
 
--- src/test/java/ru/netology/Registration.java
+++ src/test/java/ru/netology/Registration.java
@@ -10,6 +10,5 @@ public class Registration {
         private String phoneNumber;
         private String city;
 
-
     }
 }
