0bb893b1a35808ad2509c353993a2c848f124ea3
Alexandr Polyakov <PolyakovQA@yandex.ru>
2020-08-09 16:05:59 +0300

    corrected allure

--- src/test/java/ru/netology/PlanningOrderDeliveryCard.java
+++ src/test/java/ru/netology/PlanningOrderDeliveryCard.java
@@ -1,6 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -12,6 +16,16 @@ import static ru.netology.DataGenerator.getDate;
 
 
 public class PlanningOrderDeliveryCard {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("AllureSelenide");
+    }
+
     Registration.RegistrationUser user = generateUser();
     String otherDate = getDate(7);
 
